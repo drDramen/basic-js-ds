2832ee592c77241be0eef9ea95323e471c73a1ad
drDramen <vvpalchin@gmail.com>
2022-05-02 16:49:11 +0300

    feat: implement Queue task

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,20 +14,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this._queue = null;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    return this._queue;
+  }
+
+  enqueue(value) {
+    const node = new ListNode(value);
+
+    if (!this._queue) {
+      this._queue = node;
+    } else {
+      let crntNode = this._queue;
+
+      while (crntNode) {
+        if (!crntNode.next) {
+          crntNode.next = node;
+          break;
+        }
+
+        crntNode = crntNode.next;
+      }
+    }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const out = this._queue.value;
+    this._queue = this._queue.next;
+
+    return out;
   }
 }
 
